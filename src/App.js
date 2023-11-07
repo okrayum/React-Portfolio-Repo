d30042e6eb18e843e4c3f2f6139317a51e2b16a5
okrayum <morgannj71@gmail.com>
2023-11-07 13:04:35 -0700

    margin changes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="container">
-      <div>
+    <>
+      {/* <div> */}
       <Navbar />
       <Main />
-      </div>
-    </div>
+      
+      {/* </div> */}
+    </>
   );
 }
 
