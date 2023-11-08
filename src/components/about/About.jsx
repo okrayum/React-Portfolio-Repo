c38e88e5e840b8d8387850272130464ec56d5e17
okrayum <morgannj71@gmail.com>
2023-11-08 11:39:59 -0700

    skills icons component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import "./About.css";
+import SkillsGrid from '../skills/SkillsGrid';
 
 const About = () => {
   return (
-    <>
-    <div id="about" className='aboutDiv'>
-      <h1 className="aboutName">Jason Morgan</h1>
-      <div className="divider"></div>
-      <p className="aboutMe">Certified Full-Stack Web Developer with 1.5 years experience designing, constructing, and debugging responsive web applications. Experienced in a teamwork environment, using creative problem solving and team driven communication. Specializing in logic and functionality with Node.js, & JavaScript. Enthusiastic about finding a position to learn and grow in an energetic and goal driven team.</p>
+    <div className='about' id="about">
+        <h1 className="aboutName">Jason Morgan</h1>
+        <div className="divider"></div>
+        <p className="aboutMe">Certified Full-Stack Web Developer with 1.5 years experience designing, constructing, and debugging responsive web applications. Experienced in a teamwork environment, using creative problem solving and team driven communication. Specializing in logic and functionality with Node.js & JavaScript. Enthusiastic about finding a position to learn and grow in an energetic and goal driven team.</p>
+        <h2 className="aboutName">SKILLS</h2>
+        <div className="divider"></div>
+        <SkillsGrid />
     </div>
-    </>
   )
 }
 
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -7,49 +7,45 @@ import myImage from "../images/updated2.png";
 
 const Navbar = () => {
   return (
-    <div>
+    <nav className="navbar">
 
-      <nav className="navbar">
+      <div className="image">
+        <img src={myImage} alt="Myself" width="100%" />
+      </div>
 
-        <div className="image">
-          <img src={myImage} alt="Myself" width="100%" />
-        </div>
+      <ul className="nav">
 
-        <ul className="nav">
+        <li className="link">
+          <a href="#home">
+            <BiHomeAlt2 size={50} className='icon' />
+            <div>HOME</div>
+          </a>
+        </li>
 
-          <li className="link">
-            <a href="#home">
-              <BiHomeAlt2 size={50} className='icon' />
-              <div>HOME</div>
-            </a>
-          </li>
+        <li className="link">
+          <a href="#about">
+            <BsPerson size={50} className='icon' />
+            <div>ABOUT</div>
+          </a>
+        </li>
 
-          <li className="link">
-            <a href="#about">
-              <BsPerson size={50} className='icon' />
-              <div>ABOUT</div>
-            </a>
-          </li>
+        <li className="link">
+          <a href="#projects">
+            <BsEye size={50} className='icon' />
+            <div>PROJECTS</div>
+          </a>
+        </li>
 
-          <li className="link">
-            <a href="#projects">
-              <BsEye size={50} className='icon' />
-              <div>PROJECTS</div>
-            </a>
-          </li>
+        <li className="link">
+          <a href="#contact">
+            <RxEnvelopeClosed size={50} className='icon' />
+            <div>CONTACT</div>
+          </a>
+        </li>
 
-          <li className="link">
-            <a href="#contact">
-              <RxEnvelopeClosed size={50} className='icon' />
-              <div>CONTACT</div>
-            </a>
-          </li>
+      </ul>
 
-        </ul>
-
-      </nav>
-
-    </div>
+    </nav>
   );
 }
 
