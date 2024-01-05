7832bdd7e5d82d0403a50060b2046a97d359b3d8
Jason Morgan <148730648+okrayum@users.noreply.github.com>
2024-01-05 10:34:07 -0700

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <div className='about' id="about">
         <h1 className="aboutName">Jason Morgan</h1>
         <div className="divider"></div>
-        <p className="aboutMe">Certified Full-Stack Web Developer with 1.5 years experience designing, constructing, and debugging responsive web applications. Experienced in a teamwork environment, using creative problem solving and goal driven communication. Specializing in logic and functionality with Node.js & JavaScript. Enthusiastic about finding a position to learn and grow in an energetic team.</p>
+        <p className="aboutMe">Certified Full-Stack Web Developer with a strong foundation in designing, constructing, and debugging responsive web applications. My proficiency in HTML5, CSS, JavaScript, and React is coupled with a deep passion for creative problem-solving and effective team communication. I thrive in dynamic, goal-driven team settings and am eager to contribute to projects that push the boundaries of what's possible in the digital realm. Let's build something extraordinary together!</p>
         <h2 className="aboutName skills">My Skills</h2>
         <div className="divider"></div>
         <SkillsGrid />
